c114f7a452fbdae8d4ca56cdc927a3d6e21f0935
spj290 <daniel.davidsen9@hotmail.com>
2021-04-27 10:59:26 +0200

    Daniel test

--- Breakout/Program.cs
+++ Breakout/Program.cs
@@ -6,7 +6,7 @@ namespace Breakout
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Darkness my Old Friend!");
         }
     }
 }
